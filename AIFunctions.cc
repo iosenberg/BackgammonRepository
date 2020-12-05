cc64f5c6945bec5cf23bdb97022ff021acbbd72e
Esme Rubinstein <emr@thor.local>
2020-12-04 18:06:08 -0700

    more comments added, dealing with bearing off case

--- AIFunctions.cc
+++ AIFunctions.cc
@@ -56,6 +56,16 @@ public:
     bar = 0;
     opponentBar = 0;
   }
+  bool readyToBearOff(board board1){
+    int sum = 0;
+    for (int i = 0; i < 18; i++){
+      sum+= board1.boardArray[i];
+    }
+    if ((sum > 0) || (board1.bar > 0)){
+      return false;
+    }
+    return true;
+  }
 
   //function to print board for testing
   void printBoard(){
@@ -147,7 +157,7 @@ void push(StackNode** root, board data){
   stackNode->next = *root;
   *root = stackNode;
   data.printBoard();
-  cout << " pushed to stack\n";
+  cout << "pushed to stack\n";
 }
 
 //need to return like empty board and idk what that would look like.  i dont even use pop for this list so its ok
@@ -180,17 +190,25 @@ StackNode* generateMovesForSingleRoll(board board1, int roll){
       push(&possibleBoards, possibleBoard);
     }
   }
+  else if (board1.readyToBearOff(board1)){
+    if (board1.boardArray[24-roll] > 0){
+      board possibleBoard = board1.copyBoard(board1);
+      possibleBoard.boardArray[24-roll]--;
+      push(&possibleBoards, possibleBoard);
+    }
+  }
+      
   //case for not on bar
   else {
     for (int i = 0; i < 24; i++){
-      //check if where your chips are
+      //check where your chips are
       if (board1.boardArray[i] > 0){
 	//check if space roll away from that chip is open
 	if(board1.boardArray[i+roll] >= -1){
 	  //add new possible board for each open piece and open slot
 	  board possibleBoard = board1.copyBoard(board1);
-	  possibleBoard.boardArray[i+roll]++; //= board1.boardArray[i+roll]+1;
-	  possibleBoard.boardArray[i]--;//=board1.boardArray[i]-1;
+	  possibleBoard.boardArray[i+roll]++;
+	  possibleBoard.boardArray[i]--;
 	  push(&possibleBoards, possibleBoard);
 	}
       }
@@ -206,22 +224,18 @@ int main(){
   board board1;
   board1.setInitialBoard();
   board1.printBoard();
-  board board2;
-  board2 = board1.copyBoard(board1);
-  board2.printBoard();
   board board3;
-  board3 = board2.reverseBoard(board2);
+  board3 = board1.reverseBoard(board1);
   board3.printBoard();
-  if(board1.boardEqualP(board1, board2)){
+  if(board1.boardEqualP(board1, board3)){
     cout << "True" << endl;
   }
   else{
     cout << "False" << endl;
   }
-  board barBoard;
-  barBoard.bar = 1;
-  barBoard.boardArray[0] = -2;
-  generateMovesForSingleRoll(barBoard, 1);
+  board bearingOffBoard;
+  bearingOffBoard.boardArray[23] = 15;
+  generateMovesForSingleRoll(bearingOffBoard, 1);
   return 0;
 }
     
@@ -230,27 +244,3 @@ int main(){
 
 
 
-
-//using board as an array
-
-//print board - function to print board for testing
-
-//delete_all_boards - function delete all boards - this needs to happen so that in our generate all legal board moves function, we can delete options (ie: sometimes we save the move of a single dice roll if that is the only legal move, but other times we want to delete the saved single roll moves because there are legal moves for both rolls
-
-//board_equal_p - function to check if two boards are equal
-
-//reverse_board - function reverse board --> this will reverse the values of the pieces on the board. this makes it so that we can have two computers play each other pretty easily.  essentially just lets you switch players.   (if array index 3 has value -1, it will be switched to 1
-
-//copy array - function to copy array 
-
-//roll - function for roll - we should make this return the number and the frequency -- that way when we get double 3's we can have it return (3, 4) and therefore know to generate possible moves where you can move 3 four times.
-
-//functions for bearing off:
-//no_checkers_in_range_p(board, start, end) 
-//bearing_off_p(board)
-//no_checkers_outside_of_roll_p(board, roll)
-//no_checkers_on_board_p (board) 
-
-//generate_boards_for_roll(board, roll) - this will generate all possible moves for a particular board and a given roll - this means a SINGLE roll, give all legal moves
-
-//generate_moves (initial board, rolls) - this will generate all legal moves for multiple rolls and it will call the above generate_boards_for_roll function
