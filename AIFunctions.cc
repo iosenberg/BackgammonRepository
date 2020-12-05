3fe968fafdcc5967c7ec6b3e39b1b1777dc0cf58
Esme Rubinstein <emr@thor.local>
2020-12-04 17:03:09 -0700

    added comments

--- AIFunctions.cc
+++ AIFunctions.cc
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
-//initial board setup - puts 25 and 26 as the bar but this may change - not sure if we should still make a class?
-//int board[26] = {2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2, 0, 0};
 
+// dice class - creates two rolls and their frequency
 class dice {
 public:
   int roll1;
@@ -10,20 +9,25 @@ public:
   int frequency1 = 0;
   int frequency2 = 0;
   dice() {
+    //constructor calls roll() function
     roll();
   }
   void roll(){
+    //sets random values 1-6 to both rolls
     roll1 = rand()%6+1;
     roll2 = rand()%6+1;
     if(roll1 == roll2){
       //how do i delete the value in roll2? do i need to?
+      //sets frequency to 4 if doubles
       frequency1 = 4;
     }
     else{
+      //sets frequency to 1 if not doubles
       frequency1 = 1;
-      frequency2 = 2;
+      frequency2 = 1;
     }
   }
+  //function to print roll for testing
   void printRoll(){
     cout << "roll1 " << roll1; 
     cout << "roll2 " << roll2;
@@ -34,10 +38,12 @@ public:
 
 class board {
 public:
+  //initial board array (this gives a warning so maybe something wrong with this. still works though)
   int boardArray[24] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   int bar;
   int opponentBar;
-  
+
+  //function to set board to starting game baord
   void setInitialBoard(){
     boardArray[0] = 2;
     boardArray[5] = -5;
@@ -51,6 +57,7 @@ public:
     opponentBar = 0;
   }
 
+  //function to print board for testing
   void printBoard(){
     for(int i = 0; i < 24; i++){
       cout << boardArray[i] << " ";
@@ -58,7 +65,8 @@ public:
     cout << bar << " ";
     cout << opponentBar << endl;
   }
-  
+
+  //function to copy board - need this to generate possible boards
   board copyBoard(board originalBoard){
     board newBoard;
     for (int i = 0; i <24; i++){
@@ -68,7 +76,8 @@ public:
     newBoard.opponentBar = originalBoard.opponentBar;
     return newBoard;
   }
-  
+
+  //checkes if two boards are equal, this will delete boards in our list of possible boards that are duplicates, not necessary but saves some time/efficiency
   bool boardEqualP(board board1, board board2){
     bool check = true;
     int index = 0;
@@ -86,9 +95,17 @@ public:
     }
     return check;
   }
+  
   //does this make a copy and reverse? or just reverse the original? also there is a way to do this recursively but i dont know how to without saving start and end as function calls, this iterative way works though
+  //function that "reverses" board, it will reverse the array and change the sign for all elements in array
   board reverseBoard(board board1){
+    //creates copy of board that will be reversed
     board newBoard = board1.copyBoard(board1);
+    //switches the bars
+    int temp = newBoard.opponentBar;
+    newBoard.opponentBar = newBoard.bar;
+    newBoard.bar = temp;
+    //reverses the array
     int start = 0;
     int end = 23;
     while (start < end){
@@ -98,6 +115,7 @@ public:
       start++;
       end--;
     }
+    //changes sign of array elements
     for(int i = 0; i <24; i++){
       newBoard.boardArray[i] = -newBoard.boardArray[i];
     }
@@ -106,8 +124,7 @@ public:
          
 };
 
-//need to make either linked list or stack class?? for now just make generate moves function return void
-//not sure what it should take for arguments - is it a problem tha ti have roll1 and roll2?
+//class stack to hold the list of possible boards
 class StackNode {
 public:
   board data;
@@ -136,7 +153,7 @@ void push(StackNode** root, board data){
 //need to return like empty board and idk what that would look like.  i dont even use pop for this list so its ok
 //board pop(StackNode** root){
 //  if (isEmpty(*root)){
-//    return ;
+//    return NULL?;
 //  }
 //  StackNode* temp = *root;
 //  *root = (*root)->next;
@@ -146,25 +163,45 @@ void push(StackNode** root, board data){
 //}
 
 
+
+//generates all possible moves for a single roll (returns stack of board instances)
 StackNode* generateMovesForSingleRoll(board board1, int roll){
+  //create stack for possibleBoards
   StackNode* possibleBoards = NULL;
-  for (int i = 0; i < 24; i++){
-    if (board1.boardArray[i] > 0){
-      if(board1.boardArray[i+roll] >= -1){
-	board possibleBoard = board1.copyBoard(board1);
-	possibleBoard.boardArray[i+roll] = board1.boardArray[i+roll]+1;
-	possibleBoard.boardArray[i] = board1.boardArray[i]-1;
-	push(&possibleBoards, possibleBoard);
+  //check if any chips on bar
+  if (board1.bar > 0){
+    //check if chips can be taken off bar (slot is open)
+    if(board1.boardArray[roll-1] > -1){
+      //create new board where chip is taken off bar and into open slot
+      board possibleBoard = board1.copyBoard(board1);
+      possibleBoard.bar--;
+      possibleBoard.boardArray[roll-1]++;
+      //push possibleBoard onto stack
+      push(&possibleBoards, possibleBoard);
+    }
+  }
+  //case for not on bar
+  else {
+    for (int i = 0; i < 24; i++){
+      //check if where your chips are
+      if (board1.boardArray[i] > 0){
+	//check if space roll away from that chip is open
+	if(board1.boardArray[i+roll] >= -1){
+	  //add new possible board for each open piece and open slot
+	  board possibleBoard = board1.copyBoard(board1);
+	  possibleBoard.boardArray[i+roll]++; //= board1.boardArray[i+roll]+1;
+	  possibleBoard.boardArray[i]--;//=board1.boardArray[i]-1;
+	  push(&possibleBoards, possibleBoard);
+	}
       }
     }
   }
-  //something is wrong here it somehow is not using board 1 as a start board in this loop
   return possibleBoards;
 }
     
 
 
-
+//function for testing
 int main(){
   board board1;
   board1.setInitialBoard();
@@ -181,8 +218,10 @@ int main(){
   else{
     cout << "False" << endl;
   }
-
-  generateMovesForSingleRoll(board1, 1);
+  board barBoard;
+  barBoard.bar = 1;
+  barBoard.boardArray[0] = -2;
+  generateMovesForSingleRoll(barBoard, 1);
   return 0;
 }
     
