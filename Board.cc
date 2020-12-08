a4e260a520bc7df3d2173f2d69274a669ccb0288
Veronica Ayars <veronica.ayars@oberlin.edu>
2020-12-08 15:40:41 -0500

    Added simple getMoves function; included RollsList.h

--- Board.cc
+++ Board.cc
@@ -123,14 +123,28 @@ void Board::OnClick(wxMouseEvent& event) //when the mouse is clicked within the 
   //selectedsection is the index of the piece selected by the mouse click
   
   if(selectedpiece > -1 && selectedpiece < 25) {
-    // 0 is bar, 25 is white endspace, 26 is brown endspace
+    // 0 is bar, 26 is white endspace, 25 is brown endspace
     // White is positive, brown is negative
     int startspace;
-    if (selectedpiece == 0){
-      if (color == true) startspace = 26;
-      else if (color == false) startspace = 25;
+    int currentroll;
+    int counter = 0;
+    // Set start space
+    if (selectedpiece == 0){ //NOTE: we need code that prevents the player/AI from selecting anything off the bar if they have pieces on the bar.
+      if (color == true) startspace = 25;
+      else if (color == false) startspace = 0; // This is zero so if they roll a 1, the available spot is 1.
     } else startspace = selectedpiece;
-  }
+    // For each roll:
+    while(counter != 2){ // Even if someone rolls doubles, the possible moves are going to be the same
+      currentroll = currentRolls->RollsList::first();
+      if(color == true) currentroll *= -1;
+      int destination = startspace + currentroll;
+      // Set a boolean for if a player has all their pieces in their side of the board?
+      if(destination > 25) destination = 25;
+      if(destination < 1) destination = 26;
+      // Check if the spots on the board are available
+      if(boardArray[destination] < -1 || boardArray[] > 1) continue;
+      else movesList[counter] = destination;
+    }
   
     if(//selectedsection is in list of possible moves
        ) {
